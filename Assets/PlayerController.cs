6f5431421e7f649e7632e973a931eb4d10857c38
Michael O'Keefe <mokeefe00@gmail.com>
2022-10-01 14:16:44 -0700

    initial commit, set up basic scene features

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMove(Vector2 movementVector) {
+       
+
+    }
+}
